126875234d21f366c8c1d719be1b67edf736a4fa
Law <victorestrada2006@gmail.com>
2023-04-22 22:54:15 +0200

    Seleccionar elementos de HTML con Javascript

--- coneccion/coneccion.js
+++ coneccion/coneccion.js
@@ -1,3 +1,13 @@
 'use strict'
 
-console.log("Hello, world");
+const title = document.querySelector('h1');
+const parrafo = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    title, parrafo, parrafito, pid, input
+})
