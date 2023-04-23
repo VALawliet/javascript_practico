f32e94d01cba68870b47594bfa3bfb28c487d446
Law <victorestrada2006@gmail.com>
2023-04-23 23:56:57 +0200

    Usando condicionales y funciones para manipular eventos en el HTML

--- coneccion/coneccion.js
+++ coneccion/coneccion.js
@@ -5,59 +5,32 @@
 //La variable es preferible que sea una constante.
 const title = document.querySelector('h1');
 
-//Ejemplo 1
-const parrafo = document.querySelector('p');
-//Se selecciona un elemento por el nombre de ese elemento. En este caso, se selecciona la primera etiqueta p
+const input = document.querySelector('#Calculo1');
+const input2 = document.querySelector('#Calculo2');
+const btn = document.querySelector('#btrCalcular');
+const newDiv = document.querySelector('.show');
 
+function num1(){
+    
+    console.log("El valor del input es " + input.value);
 
+}
 
-//Ejemplo 2
-const parrafito = document.querySelector('.parrafito');
-//Se selecciona el elemento por clase. Cabe recalcar que, con querySelector, se llama a las clases e identificadores a como se 
-//haría normalmente en css
+function num2(){
+    
+    console.log("El valor del segundo input es " +  input2.value);
+}
 
-//Ejemplo 3
-const pid = document.querySelector('#pid');
-//Seleccionar el elemento por el id usando la sintaxis de css
+function Calculate(){
+    let value1 = Number(input.value);
+    let value2 = Number(input2.value);
 
-const input = document.querySelector('input');
+    if(isNaN(value1) == true || isNaN(value2)){
+        console.log("No se pueden realizar calculos con valores no númericos");
+        newDiv.innerText = 'No se puede realizar una suma de valores no númericos.'
 
-
-//Modificación del HTML interno de una variable
-title.innerHTML = "Patito <br> Feo";
-
-//Modificación del texto interno de una variable
-title.innerText = "Patito <br> Feo";
-
-
-/* console.log(title.getAttribute('class'));
-title.setAttribute('class', 'rojo');
-console.log(title.getAttribute('class')); */
-
-//Para seleccionar especificamente las clases de un elemento se utiliza:
-// .classList
-
-//Añadiendole .add a classList podemos agregarle una clase al elemento
-title.classList.add("Verde");
-
-//Añadiendo .remove a classList podemos quitarle una clase a un elemento.
-title.classList.remove('platzilg');
-
-//Para asignarle un valor por defecto a los inputs se utiliza .value = x
-input.value = "453";
-
-//Para crear un elemento HTML desde Javascript se utiliza document.createElement('nombre de etiqueta');
-const img = document.createElement('img');
-
-img.classList.add("Smth");
-
-console.log(img.getAttribute('class'));
-
-//Para añadirle un atributo a una etiqueta de HTMl se utiliza .setAtribute('nombre del atributo', 'Valor del atributo');
-img.setAttribute('src', 'https://yt3.googleusercontent.com/ytc/AGIKgqMF2M0gIVQeSen6lfXCe8P281NMD6pgS6YrOYmk-g=s900-c-k-c0x00ffffff-no-rj');
-console.log(img.getAttribute('src'));
-
-pid.innerText = "";
-
-//.append se utiliza para agregar un elemento HTML dentro de otro.
-pid.append(img)
+    }else{
+        console.log(`La respuesta de la suma es ${value1 + value2}`);
+        newDiv.innerText = `La respuesta a la suma es ${value1 + value2}`;
+    }
+}
