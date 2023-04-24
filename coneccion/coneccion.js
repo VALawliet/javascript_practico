84c1e6eb032d95d8d1fbb664149e1fc303ab2ab0
Law <victorestrada2006@gmail.com>
2023-04-24 21:00:42 +0200

    Ejercicio de evento de scroll usando window

--- coneccion/coneccion.js
+++ coneccion/coneccion.js
@@ -7,11 +7,27 @@ const title = document.querySelector('h1');
 
 const input = document.querySelector('#Calculo1');
 const input2 = document.querySelector('#Calculo2');
-const btn = document.querySelector('#btrCalcular');
+const btn = document.querySelector('#btnCalcular');
+const form = document.querySelector("#form");
 
 //Seleccionando un nuevo elemento desde Javascript para
 //Utilizarlo en el código.
 const newDiv = document.querySelector('.show');
+
+window.addEventListener('scroll', scrollin);
+
+function scrollin(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        newDiv.className = 'smth1';
+    }else{
+        newDiv.className = "";
+    }
+}
+
+//.addEventListener('nombre del evento', función a ejecutar sin parentesis)
+//form.addEventListener('submit', sumarInputValues);
+//La función de arriba no me parece la mejor para usar; prefiero la de abajo.
+btn.addEventListener('click',sumarInputValues);
 
 function num1(){
     
@@ -24,7 +40,13 @@ function num2(){
     console.log("El valor del segundo input es " +  input2.value);
 }
 
-function Calculate(){
+function sumarInputValues(event){
+    //console.log({event});
+
+    //event.preventDefault() previene que el evento haga su función por defecto.
+    //Esto es útil cuando queremos que un submit no haga request
+    //A otra url
+    //event.preventDefault()
 
     //La función Number() nos permite transformar a números todo lo que el usuario escriba.
     let value1 = Number(input.value);
